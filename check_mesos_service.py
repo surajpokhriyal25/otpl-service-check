2e53226585282b5406f46eb7a0546a191f8141c3
Chris Pennello <cpennello@opentable.com>
2016-06-15 11:07:44 -0700

    Updates based on Steven's suggestions.
    - Produces output even on OK.
    - Logs which service endpoints had problems.
    - Now errors on unexpected healthcheck endpoint response code.
    - Improved formatting.

--- check_mesos_service.py
+++ check_mesos_service.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import time
 import traceback
 from argparse import ArgumentParser
 from collections import namedtuple
@@ -10,10 +11,11 @@ import requests
 
 discotimeout = 4 # In seconds.
 tokenkey = 'server-token'
-resplim = 128 # Response output character limit.
+resplim = 64 # Response output character limit; only printed if not OK.
+
+Result = namedtuple('Result', ('code', 'message'))
 
 class Main(object):
-  Problem = namedtuple('Problem', ('code', 'message'))
 
   # Parse arguments.
   def __init__(self):
@@ -67,43 +69,58 @@ class Main(object):
         count += 1
     return count
 
-  @classmethod
-  def make_problem(cls, code, topic, message):
-    state = {2: 'critical', 1: 'warning'}[code]
-    return cls.Problem(code, '%s %s: %s' % (topic, state, message))
+  @staticmethod
+  def make_result(code, topic, message):
+    state = {2: 'critical', 1: 'warning', 0: 'ok'}[code]
+    return Result(code, '%s %s: %s' % (topic, state, message))
 
-  def make_announcement_problem(self, code, count):
-    msg = '%s (crit./warn thresh. %s/%s)' % (
+  @classmethod
+  def make_result_with_announcement(cls, code, topic, ann, message):
+    msg = '%s\nservice URI %s' % (message, ann['serviceUri'])
+    return cls.make_result(code, topic, msg)
+
+  def make_announcement_result(self, code, count):
+    msg = '%s\ncrit./warn thresh.: %s/%s' % (
       count, self.args.critical, self.args.warn)
-    return self.make_problem(code, 'announcements', msg)
+    return self.make_result(code, 'announcements', msg)
 
   @classmethod
-  def make_response_problem(cls, code, status_code, text):
-    if len(text) > resplim: text = text[:resplim] + '...'
-    msg = '%s from endpoint\n%s' % (status_code, text)
-    return cls.make_problem(code, 'health', msg)
+  def make_response_result(cls, code, ann, status_code, duration, text):
+    msg = '%s from endpoint\nduration %.3fs' % (status_code, duration)
+    if code != 0:
+      if len(text) > resplim: text = text[:resplim] + '...'
+      msg += '\n' + text
+    return cls.make_result_with_announcement(code, 'health', ann, msg)
 
-  def make_timeout_problem(self, type):
-    return self.make_problem(2, '%s timeout' % type,
-      'thresh. %.2f' % self.args.timeout)
+  def make_timeout_result(self, ann, type):
+    return self.make_result_with_announcement(
+      2, '%s timeout' % type, ann, 'thresh. %.3f' % self.args.timeout)
 
-  # Return Problem or None if ok.
-  def check_endpoint(self, announcement):
-    url = urljoin(announcement['serviceUri'], self.args.endpoint)
+  def check_endpoint(self, ann):
+    serviceuri = ann['serviceUri']
+    url = urljoin(serviceuri, self.args.endpoint)
+    start = time.time()
     try:
       resp = requests.get(url, timeout=self.args.timeout)
+      duration = time.time() - start
       code = resp.status_code // 100
-      if code == 5:
-        return self.make_response_problem(2, resp.status_code, resp.text)
+      if code == 2:
+        return self.make_response_result(
+          0, ann, resp.status_code, duration, resp.text)
       if code == 4:
-        return self.make_response_problem(1, resp.status_code, resp.text)
+        return self.make_response_result(
+          1, ann, resp.status_code, duration, resp.text)
+      if code == 5:
+        return self.make_response_result(
+          2, ann, resp.status_code, duration, resp.text)
+      raise Exception('unexpected status code', resp.status_code)
     except requests.exceptions.ConnectTimeout:
-      return self.make_timeout_problem('connect')
+      return self.make_timeout_result(ann, 'connect')
     except requests.exceptions.ReadTimeout:
-      return self.make_timeout_problem('read')
+      return self.make_timeout_result(ann, 'read')
     except requests.exceptions.RequestException:
-      return self.make_problem(2, 'health',
-        'unhandled exception\n' + traceback.format_exc())
+      return self.make_result_with_announcement(2, 'health',
+        ann, 'unhandled exception\n' + traceback.format_exc())
 
   def run(self):
     try:
@@ -113,23 +130,26 @@ class Main(object):
       print traceback.format_exc()
       return 3
 
-    # Will contain Problem instances.
-    bad = []
+    # Will contain Result instances.
+    results = []
 
     count = self.count_announcements(announcements)
     if count < self.args.critical:
-      bad.append(self.make_announcement_problem(2, count))
+      results.append(self.make_announcement_result(2, count))
     elif count < self.args.warn:
-      bad.append(self.make_announcement_problem(1, count))
+      results.append(self.make_announcement_result(1, count))
+    else:
+      results.append(self.make_announcement_result(0, count))
 
     for ann in announcements:
-      problem = self.check_endpoint(ann)
-      if problem is not None: bad.append(problem)
+      res = self.check_endpoint(ann)
+      if res is not None: results.append(res)
 
-    bad.sort(reverse=True) # Worst problems first.
-    for problem in bad: print problem.message
-
-    # Return with worst code if there are any, else 0 for success.
-    return bad[0].code if bad else 0
+    # Print worst results first and return with worst code.
+    results.sort(reverse=True)
+    for res in results:
+      print res.message
+      print '---'
+    return results[0].code
 
 sys.exit(Main().run())
